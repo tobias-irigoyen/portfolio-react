49b19e8e55063444d42ebdc8fc27c3fd8398e0bd
PC <tobias.irigoyen@gmail.com>
2026-03-28 03:14:18 -0300

    (projects): delete scroll animation

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -85,10 +85,7 @@ function ProjectCard({
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: 30 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, margin: '-40px' }}
-      transition={{ duration: 0.5, delay: index * 0.07 }}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       onClick={onClick}
